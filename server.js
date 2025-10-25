fed77708f68ad3a721285fc9e7f65a9f08b5abcd
Ivy <ivyzhao335@gmail.com>
2025-10-24 21:05:16 -0400

    update

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
 // Your server code goes here. Host your server on localhost:3000
 console.log('Node is running')
+
+const http = require('http');
+const fs = require('fs'); //file system module
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/' || req.url === '/index.html') {
+        fs.readFile('index.html', (err, data) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Error loading file.');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data)
+            }
+        })
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
